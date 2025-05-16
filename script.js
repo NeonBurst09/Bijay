199df9d3e8a681979ba0b69912339a5751c16404
NeonBurst09 <uchia835@gmail.com>
2025-05-16 19:41:14 +0545

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function showWeather() {
-  const conditions = ["Sunny", "Rainy", "Thunderstorm", "Clear"];
-  const random = conditions[Math.floor(Math.random() * conditions.length)];
-  document.getElementById("weatherOutput").textContent = `Current weather: ${random}`;
-}
